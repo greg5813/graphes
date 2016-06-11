b8af74329191040dc31adc838a5c0edeb8fc8078
FelixN7 <felix9117@gmail.com>
2016-06-11 15:07:19 +0200

    fin des algos, reste a ranger entierement le code

--- interface.ml
+++ interface.ml
@@ -77,7 +77,7 @@ let rec extraction dag l l_exec l_res =
     | [] -> l_res
     | t::q -> if(appartient (pred dag t) (List.flatten l_exec))
               then extraction dag q l_exec (inserer_trie t l_res)
-              else l_res;;
+              else extraction dag q l_exec l_res;;
 
 let rec retirer_aux x l =
     match l with
@@ -103,24 +103,72 @@ let rec marquage_aux dag l1 l2 =
                 marquage_aux dag (fold_pred (fun v laux -> if( appartient (succ dag v) (l2@[t]))
 						    then (laux@[v]) 
                                                     else laux) dag t q) (l2@[t])
-              end;; 
+              end;;
+ 
 let marquage dag =
     Mark.clear dag;
     let puits = fold_vertex (fun v l -> if ( (out_degree dag v)==0) 
 				     then l@[v] else l) dag [] in
     marquage_aux dag puits [];;
+
+let rec k_elem l k l_res =
+    if k = 0 then l_res
+    else
+      match l with
+      | [] -> l_res
+      | t::q -> k_elem q (k-1) (l_res@[t]);;
+
+let rec ordonnanceur_avec_heuristique_aux r dag l_aux ll =
+    if ((List.length l_aux) = 0) 
+    then ll
+    else
+      let l = k_elem (extraction dag l_aux ll []) r [] in 
+      ordonnanceur_avec_heuristique_aux r dag (retirer l l_aux) (ll@[l]);;
+
+let ordonnanceur_avec_heuristique r dag =
+    marquage dag;
+    let l_topo = tri_topologique dag in
+    ordonnanceur_avec_heuristique_aux r dag l_topo [];; 
     
+let rec retirer_elem_memoire l k m l_res =
+    if ((k=0) || (m=0)) then l_res
+    else
+      match l with
+      | [] -> l_res
+      | t::q -> let memory = Vertex.memory (V.label t) in
+                if (memory<=m) then retirer_elem_memoire q (k-1) (m-memory) (l_res@[t])
+                else retirer_elem_memoire q k m l_res;;
 
-let rec ordonnanceur_contrainte_memoire_aux r dag lv_tp ll lc = 
-  match lv_tp with 
-  | [] -> ll@[lc]
-  | t::q -> if ((appartient (pred dag t) (List.flatten ll)) && ((List.length lc)<r)) then
-                ordonnanceur_contrainte_memoire_aux r dag q ll (lc@[t])
-            else
-                ordonnanceur_contrainte_memoire_aux r dag q (ll@[lc]) [t];;
+let rec ordonnanceur_contrainte_memoire_aux r m dag l_aux ll =
+    if ((List.length l_aux) = 0) 
+    then ll
+    else
+      let l = retirer_elem_memoire (extraction dag l_aux ll []) r m [] in 
+      ordonnanceur_contrainte_memoire_aux r m dag (retirer l l_aux) (ll@[l]);;
 
-let ordonnanceur_contrainte_memoire r dag = 
-  let lv_tp = tri_topologique dag in
-    ordonnanceur_contrainte_memoire_    aux r dag lv_tp [] [];;
+let ordonnanceur_contrainte_memoire r m dag =
+    iter_vertex (fun v -> if ((Vertex.memory (V.label v)) > m) then failwith ("Memoire insuffisante") else ()) dag;
+    marquage dag;
+    let l_topo = tri_topologique dag in
+    ordonnanceur_contrainte_memoire_aux r m dag l_topo [];;
 
+let rec ajouter x l =
+    match l with
+    | [] -> [x]
+    | t::q -> if(t=x) then l else t::(ajouter x q);;
+
+let rec marquage_bonus_aux dag v l_res =
+  if ((out_degree dag v)=0) then l_res
+  else fold_succ (fun v_aux l -> marquage_bonus_aux dag v_aux (ajouter v_aux l)) dag v l_res;;
+    
+let marquage_bonus dag =
+    Mark.clear dag;
+    iter_vertex (fun v -> let l = marquage_bonus_aux dag v [] in
+                          Mark.set v (List.length l)) dag;;
+
+let ordonnanceur_contrainte_memoire_bonus r m dag =
+    iter_vertex (fun v -> if ((Vertex.memory (V.label v)) > m) then failwith ("Memoire insuffisante") else ()) dag;
+    marquage_bonus dag;
+    let l_topo = tri_topologique dag in
+    ordonnanceur_contrainte_memoire_aux r m dag l_topo [];;
 
