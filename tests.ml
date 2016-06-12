50ddaae176dbc74caa83ca8aaf1d49c826ebeab5
greg5813 <gregoire.martini@free.fr>
2016-06-12 11:43:30 +0200

    toujours des test

--- tests.ml
+++ tests.ml
@@ -1,50 +1,64 @@
-List.length (ordonnanceur_sans_heuristique 1 dag1);;
-List.length (ordonnanceur_sans_heuristique 2 dag1);;
-List.length (ordonnanceur_sans_heuristique 3 dag1);;
-List.length (ordonnanceur_sans_heuristique 4 dag1);;
-List.length (ordonnanceur_sans_heuristique 5 dag1);;
+print_endline "ordonnanceur sans heuristique";;
 
-List.length (ordonnanceur_sans_heuristique 1 dag2);;
-List.length (ordonnanceur_sans_heuristique 2 dag2);;
-List.length (ordonnanceur_sans_heuristique 3 dag2);;
-List.length (ordonnanceur_sans_heuristique 4 dag2);;
-List.length (ordonnanceur_sans_heuristique 5 dag2);;
+print_endline "	dag1";;
+print_string "	  r=1";; let t = List.length (ordonnanceur_sans_heuristique 1 dag1);;
+print_string "	  r=2";; let t = List.length (ordonnanceur_sans_heuristique 2 dag1);;
+print_string "	  r=3";; let t = List.length (ordonnanceur_sans_heuristique 3 dag1);;
+print_string "	  r=4";; let t = List.length (ordonnanceur_sans_heuristique 4 dag1);;
+print_string "	  r=5";; let t = List.length (ordonnanceur_sans_heuristique 5 dag1);;
 
-List.length (ordonnanceur_sans_heuristique 1 dag3);;
-List.length (ordonnanceur_sans_heuristique 2 dag3);;
-List.length (ordonnanceur_sans_heuristique 3 dag3);;
-List.length (ordonnanceur_sans_heuristique 4 dag3);;
-List.length (ordonnanceur_sans_heuristique 5 dag3);;
+print_endline "	dag2";;
+print_string "	  r=1";; let t = List.length (ordonnanceur_sans_heuristique 1 dag2);;
+print_string "	  r=2";; let t = List.length (ordonnanceur_sans_heuristique 2 dag2);;
+print_string "	  r=3";; let t = List.length (ordonnanceur_sans_heuristique 3 dag2);;
+print_string "	  r=4";; let t = List.length (ordonnanceur_sans_heuristique 4 dag2);;
+print_string "	  r=5";; let t = List.length (ordonnanceur_sans_heuristique 5 dag2);;
 
-List.length (ordonnanceur_sans_heuristique 1 dag4);;
-List.length (ordonnanceur_sans_heuristique 2 dag4);;
-List.length (ordonnanceur_sans_heuristique 3 dag4);;
-List.length (ordonnanceur_sans_heuristique 4 dag4);;
-List.length (ordonnanceur_sans_heuristique 5 dag4);;
+print_endline " dag3";;
+print_string "	  r=1";; let t = List.length (ordonnanceur_sans_heuristique 1 dag3);;
+print_string "	  r=2";; let t = List.length (ordonnanceur_sans_heuristique 2 dag3);;
+print_string "	  r=3";; let t = List.length (ordonnanceur_sans_heuristique 3 dag3);;
+print_string "	  r=4";; let t = List.length (ordonnanceur_sans_heuristique 4 dag3);;
+print_string "	  r=5";; let t = List.length (ordonnanceur_sans_heuristique 5 dag3);;
+
+print_endline " dag4";;
+print_string "	  r=1";; let t = List.length (ordonnanceur_sans_heuristique 1 dag4);;
+print_string "	  r=2";; let t = List.length (ordonnanceur_sans_heuristique 2 dag4);;
+print_string "	  r=3";; let t = List.length (ordonnanceur_sans_heuristique 3 dag4);;
+print_string "	  r=4";; let t = List.length (ordonnanceur_sans_heuristique 4 dag4);;
+print_string "	  r=5";; let t = List.length (ordonnanceur_sans_heuristique 5 dag4);;
+
+print_endline "";;
+print_endline "ordonnanceur avec heuristique";;
+
+print_endline "	dag1";;
+print_string "	  r=1";; let t = List.length (ordonnanceur_avec_heuristique 1 dag1);;
+print_string "	  r=2";; let t = List.length (ordonnanceur_avec_heuristique 2 dag1);;
+print_string "	  r=3";; let t = List.length (ordonnanceur_avec_heuristique 3 dag1);;
+print_string "	  r=4";; let t = List.length (ordonnanceur_avec_heuristique 4 dag1);;
+print_string "	  r=5";; let t = List.length (ordonnanceur_avec_heuristique 5 dag1);;
+
+print_endline "	dag2";;
+print_string "	  r=1";; let t = List.length (ordonnanceur_avec_heuristique 1 dag2);;
+print_string "	  r=2";; let t = List.length (ordonnanceur_avec_heuristique 2 dag2);;
+print_string "	  r=3";; let t = List.length (ordonnanceur_avec_heuristique 3 dag2);;
+print_string "	  r=4";; let t = List.length (ordonnanceur_avec_heuristique 4 dag2);;
+print_string "	  r=5";; let t = List.length (ordonnanceur_avec_heuristique 5 dag2);;
+
+print_endline " dag3";;
+print_string "	  r=1";; let t = List.length (ordonnanceur_avec_heuristique 1 dag3);;
+print_string "	  r=2";; let t = List.length (ordonnanceur_avec_heuristique 2 dag3);;
+print_string "	  r=3";; let t = List.length (ordonnanceur_avec_heuristique 3 dag3);;
+print_string "	  r=4";; let t = List.length (ordonnanceur_avec_heuristique 4 dag3);;
+print_string "	  r=5";; let t = List.length (ordonnanceur_avec_heuristique 5 dag3);;
+
+print_endline " dag4";;
+print_string "	  r=1";; let t = List.length (ordonnanceur_avec_heuristique 1 dag4);;
+print_string "	  r=2";; let t = List.length (ordonnanceur_avec_heuristique 2 dag4);;
+print_string "	  r=3";; let t = List.length (ordonnanceur_avec_heuristique 3 dag4);;
+print_string "	  r=4";; let t = List.length (ordonnanceur_avec_heuristique 4 dag4);;
+print_string "	  r=5";; let t = List.length (ordonnanceur_avec_heuristique 5 dag4);;
 
 
-List.length (ordonnanceur_avec_heuristique 1 dag1);;
-List.length (ordonnanceur_avec_heuristique 2 dag1);;
-List.length (ordonnanceur_avec_heuristique 3 dag1);;
-List.length (ordonnanceur_avec_heuristique 4 dag1);;
-List.length (ordonnanceur_avec_heuristique 5 dag1);;
-
-List.length (ordonnanceur_avec_heuristique 1 dag2);;
-List.length (ordonnanceur_avec_heuristique 2 dag2);;
-List.length (ordonnanceur_avec_heuristique 3 dag2);;
-List.length (ordonnanceur_avec_heuristique 4 dag2);;
-List.length (ordonnanceur_avec_heuristique 5 dag2);;
-
-List.length (ordonnanceur_avec_heuristique 1 dag3);;
-List.length (ordonnanceur_avec_heuristique 2 dag3);;
-List.length (ordonnanceur_avec_heuristique 3 dag3);;
-List.length (ordonnanceur_avec_heuristique 4 dag3);;
-List.length (ordonnanceur_avec_heuristique 5 dag3);;
-
-List.length (ordonnanceur_avec_heuristique 1 dag4);;
-List.length (ordonnanceur_avec_heuristique 2 dag4);;
-List.length (ordonnanceur_avec_heuristique 3 dag4);;
-List.length (ordonnanceur_avec_heuristique 4 dag4);;
-List.length (ordonnanceur_avec_heuristique 5 dag4);;
-
+print_endline "ordonnanceur avec contrainte mémoire avec r de 1 à 5";;
 
