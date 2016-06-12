d5933249f4a9805b1a92a9d8c3f9a9e6defca9a7
greg5813 <gregoire.martini@free.fr>
2016-06-12 11:26:05 +0200

    tests en cours

---
+++ tests.ml
@@ -0,0 +1,50 @@
+List.length (ordonnanceur_sans_heuristique 1 dag1);;
+List.length (ordonnanceur_sans_heuristique 2 dag1);;
+List.length (ordonnanceur_sans_heuristique 3 dag1);;
+List.length (ordonnanceur_sans_heuristique 4 dag1);;
+List.length (ordonnanceur_sans_heuristique 5 dag1);;
+
+List.length (ordonnanceur_sans_heuristique 1 dag2);;
+List.length (ordonnanceur_sans_heuristique 2 dag2);;
+List.length (ordonnanceur_sans_heuristique 3 dag2);;
+List.length (ordonnanceur_sans_heuristique 4 dag2);;
+List.length (ordonnanceur_sans_heuristique 5 dag2);;
+
+List.length (ordonnanceur_sans_heuristique 1 dag3);;
+List.length (ordonnanceur_sans_heuristique 2 dag3);;
+List.length (ordonnanceur_sans_heuristique 3 dag3);;
+List.length (ordonnanceur_sans_heuristique 4 dag3);;
+List.length (ordonnanceur_sans_heuristique 5 dag3);;
+
+List.length (ordonnanceur_sans_heuristique 1 dag4);;
+List.length (ordonnanceur_sans_heuristique 2 dag4);;
+List.length (ordonnanceur_sans_heuristique 3 dag4);;
+List.length (ordonnanceur_sans_heuristique 4 dag4);;
+List.length (ordonnanceur_sans_heuristique 5 dag4);;
+
+
+List.length (ordonnanceur_avec_heuristique 1 dag1);;
+List.length (ordonnanceur_avec_heuristique 2 dag1);;
+List.length (ordonnanceur_avec_heuristique 3 dag1);;
+List.length (ordonnanceur_avec_heuristique 4 dag1);;
+List.length (ordonnanceur_avec_heuristique 5 dag1);;
+
+List.length (ordonnanceur_avec_heuristique 1 dag2);;
+List.length (ordonnanceur_avec_heuristique 2 dag2);;
+List.length (ordonnanceur_avec_heuristique 3 dag2);;
+List.length (ordonnanceur_avec_heuristique 4 dag2);;
+List.length (ordonnanceur_avec_heuristique 5 dag2);;
+
+List.length (ordonnanceur_avec_heuristique 1 dag3);;
+List.length (ordonnanceur_avec_heuristique 2 dag3);;
+List.length (ordonnanceur_avec_heuristique 3 dag3);;
+List.length (ordonnanceur_avec_heuristique 4 dag3);;
+List.length (ordonnanceur_avec_heuristique 5 dag3);;
+
+List.length (ordonnanceur_avec_heuristique 1 dag4);;
+List.length (ordonnanceur_avec_heuristique 2 dag4);;
+List.length (ordonnanceur_avec_heuristique 3 dag4);;
+List.length (ordonnanceur_avec_heuristique 4 dag4);;
+List.length (ordonnanceur_avec_heuristique 5 dag4);;
+
+
